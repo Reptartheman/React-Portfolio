0ec9cadcdc9143b384f79de667acfd0f5ac31002
Reptartheman <117610334+Reptartheman@users.noreply.github.com>
2023-04-17 22:07:38 -0400

    init

---
+++ myportfolio/src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Header from './Header.css';
+
+
+export default function Header() {
+    return (
+        <header className='header'>
+            <h1>
+            Jeff Moro: Web Developer, Audio Engineer & Musician.
+            </h1>
+        </header>
+    )
+}
